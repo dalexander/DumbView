b6681e903e1b119d8e3b05f1aee8a1858113723f
David Alexander <dalexander@ucla.edu>
2013-04-01 10:38:05 -0700

    Make window spec grokking work better
    [Tracking bug 22690]

--- DumbView/Window.py
+++ DumbView/Window.py
@@ -7,17 +7,27 @@ def windowFromString(windowString):
     splitOnColon = windowString.split(":")
     if len(splitOnColon) < 2:
         key = 1
+        rest = windowString
     else:
         key, rest = splitOnColon
     if "-" not in rest:
         refPos = int(rest)
-        start = refPos - 10
-        end   = refPos + 10
+        start = refPos - 20
+        end   = refPos + 20
     else:
         start, end = map(int, rest.split("-"))
     assert end >= start
-    return Window(key, start, end)
+    return Window(grok(key), start, end)
 
 def windowFromGffString(windowString):
     w = windowFromString(windowString)
     return w._replace(start=w.start-1)
+
+def integerValue(s):
+    try:
+        return int(s)
+    except:
+        return None
+
+def grok(s):
+    return integerValue(s) or s
