004571e76db426aff8205a1c4afec2666bd8e976
David Alexander <dalexander@ucla.edu>
2013-04-02 09:20:23 -0700

    Adjust requirements

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name = "DumbView",
-    version="0.1.0",
+    version="0.2.0",
     author="Pacific Biosciences",
     author_email="dalexander@pacificbiosciences.com",
     scripts = ["bin/dumbview"],
     packages = find_packages(),
     zip_safe = False,
     install_requires=[
-        "pbcore >= 0.5.0",
+        "pbcore >= 0.6.0",
         "numpy >= 1.6.0",
-        "h5py >= 1.3.0"
+        "h5py >= 2.0.1"
         ]
     )
