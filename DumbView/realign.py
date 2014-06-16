cf078af3dfa8eb6306ea0f534c678a5881bf1b1d
David Alexander <dalexander@pacificbiosciences.com>
2014-06-16 08:56:22 -0700

    Realign the transcript as well

--- DumbView/realign.py
+++ DumbView/realign.py
@@ -8,89 +8,83 @@
 #                 XX          XX
 #
 # Iterate until convergence.
-#
-# Also could consider alternative, more aggressive (rewritePrime)
-#
-# Rewrite rule 1': XX    ===>  XX
-#                  Y-          -Y
-#
-# Rewrite rule 2': Y-    ===>  -Y
-#                  XX          XX
 
 def toString(lst):
     return "".join(lst)
 
-def rewrite(t, q):
-    assert len(t) == len(q) == 2
+def rewrite(t, q, x):
+    assert len(t) == len(q) == len(x) == 2
     t0, t1 = t
     q0, q1 = q
+    x0, x1 = x
     if (t0 == t1 == q0) and (q1 == "-"):
-        return (t, q1 + q0)
+        return (t, q1 + q0, x1 + x0)
     elif (q0 == q1 == t0) and (t1 == "-"):
-        return (t1 + t0, q)
+        return (t1 + t0, q, x1 + x0)
     else:
-        return (t, q)
+        return (t, q, x)
 
-def rewritePrime(t, q):
-    # Allow realign around base mismatch in t/q
-    assert len(t) == len(q) == 2
-    t0, t1 = t
-    q0, q1 = q
-    if (t0 == t1) and (q1 == "-"):
-        return (t, q1 + q0)
-    elif (q0 == q1) and (t1 == "-"):
-        return (t1 + t0, q)
-    else:
-        return (t, q)
-
-
-def realign(t, q, rewrite=rewrite):
-    assert len(t) == len(q)
+def realign(t, q, x):
+    assert len(t) == len(q) == len(x)
     t = list(t)
     q = list(q)
+    x = list(x)
 
-    def rewriteScan(t, q):
-        for i in xrange(len(t)-2):
-            t[i:i+2], q[i:i+2] = rewrite(t[i:i+2], q[i:i+2])
+    def rewriteScan(t, q, x):
+        origX = x[:]
+        for i in xrange(len(t)-2,-1,-1):
+            t[i:i+2], q[i:i+2], x[i:i+2] = rewrite(t[i:i+2], q[i:i+2], x[i:i+2])
+        changed = (x != origX)
+        return changed
 
-    oldT, oldQ = None, None
-    while (oldT, oldQ) != (t, q):
-        oldT, oldQ = t[:], q[:]
-        #print toString(t), toString(q)
-        rewriteScan(t, q)
+    goAgain = True
+    while goAgain:
+        print "Scan"
+        goAgain = rewriteScan(t, q, x)
 
-    return (toString(t), toString(q))
+    return (toString(t), toString(q), toString(x))
 
-
-def realignPrime(t, q):
-    return realign(t, q, rewrite=rewritePrime)
 
 
 def test_realignment():
     #from nose import assert_equals as EQ
 
     assert realign("AAAAAA",
-                   "AAA-AA") == \
+                   "AAA-AA",
+                   "MMMDMM") == \
                   ('AAAAAA',
-                   '-AAAAA')
+                   '-AAAAA',
+                   'DMMMMM')
 
     assert realign("A-AAAA",
-                   "AAAAAA") == \
+                   "AAAAAA",
+                   "MIMMMM") == \
                   ('-AAAAA',
-                   'AAAAAA')
+                   'AAAAAA',
+                   'IMMMMM')
 
-    # Doesn't realign over base not matching the homopolymer, unless we
-    # use the prime rules
-
+    # Doesn't realign over base not matching the homopolymer
     assert realign("GATTTACA",
-                   "GAG-TACA") == \
+                   "GAG-TACA",
+                   "MMRIMMMM") == \
                   ('GATTTACA',
-                   'GAG-TACA')
+                   'GAG-TACA',
+                   'MMRIMMMM')
 
-    assert realignPrime("GATTTACA",
-                        "GAG-TACA") == \
-                       ('GATTTACA',
-                        'GA-GTACA')
+    assert realign("AAAAAA",
+                   "AAA--A",
+                   "MMMIIM") == \
+                  ('AAAAAA',
+                   '--AAAA',
+                   'IIMMMM')
+
+    assert realign("AAAAAA",
+                   "A-AA-A",
+                   "MIMMIM") == \
+                  ('AAAAAA',
+                   '--AAAA',
+                   'IIMMMM')
+
 
 
 if __name__ == '__main__':
